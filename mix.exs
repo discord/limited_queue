ee0de7424beb4265270df47527a493a3cc5afa9d
Yuliy Pisetsky <yuliy.pisetsky@discordapp.com>
2023-07-07 10:20:20 -0700

    [limited_queue] Add filter function (#1)
    Also bump to 0.1.1 and fix the CI.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule LimitedQueue.Mixfile do
   def project do
     [
       app: :limited_queue,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       elixirc_options: [warnings_as_errors: ci?()],
